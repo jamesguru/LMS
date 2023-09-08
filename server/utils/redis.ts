c2fdba51fbb32334157c082e32b13a8e712f5f36
unknown <jameskagunga2022@gmail.com>
2023-09-08 02:54:37 -0700

    added changes

--- server/utils/redis.ts
+++ server/utils/redis.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 const redisClient = () => {
     if (process.env.REDIS_URL) {
         console.log('Redis connected')
-        return process.env.REDIS_URL
+        return process.env.REDIS_URL 
     }
     throw new Error('Redis connection failed')
 }
