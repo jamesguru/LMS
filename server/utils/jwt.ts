36bde8265d331b1b9f6abbebb2387a955460d1c8
unknown <jameskagunga2022@gmail.com>
2023-09-12 05:26:54 -0700

    added changes

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,18 +1,57 @@
 require("dotenv").config();
-import {Response} from "express";
-import {IUser} from "../models/user.model";
-import {redis} from "./redis";
+import { Response } from "express";
+import { IUser } from "../models/user.model";
+import { redis } from "./redis";
 
-export interface ITokenOptions{
-    expires:Date;
-    maxAge:number;
-    httpOnly:boolean;
-    sameSite:'lax' | 'strict' | 'none' | undefined;
+export interface ITokenOptions {
+    expires: Date;
+    maxAge: number;
+    httpOnly: boolean;
+    sameSite: 'lax' | 'strict' | 'none' | undefined;
     secure?: boolean;
 }
 
-export const sendToken = (user:IUser,statusCode:number,res:Response)=>{
+export const sendToken = (user: IUser, statusCode: number, res: Response) => {
     const accessToken = user.SignAccessToken();
     const refreshToken = user.SigneRefreshToken();
+
+    // upload session to redis
+
+    redis.set(user._id,JSON.stringify(user) as any)
+
+    //parse environment variables t integrates with failback values
+
+    const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300', 10);
+    const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200', 10)
+
+    //options for cookies
+
+    const accessTokenOptions: ITokenOptions = {
+        expires: new Date(Date.now() + accessTokenExpire * 100),
+        maxAge: accessTokenExpire * 1000,
+        httpOnly: true,
+        sameSite: 'lax'
+    };
+
+    const refreshTokenOptions: ITokenOptions = {
+        expires: new Date(Date.now() + refreshTokenExpire * 100),
+        maxAge: refreshTokenExpire * 1000,
+        httpOnly: true,
+        sameSite: 'lax'
+    };
+
+
+    // only set secure to true in production
+
+    if (process.env.NODE_ENV === 'production') {
+        accessTokenOptions.secure = true;
+    }
+
+    res.status(statusCode).json({
+        success: true,
+        user,
+        accessToken
+    })
+
 }
 
