d60dc676bdf6ffa67f185e34ff693a06d4919f25
unknown <jameskagunga2022@gmail.com>
2023-10-05 05:40:28 -0700

    added changes

--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { editCourse, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
-
 
 courseRouter.post("/create-course", isAuthenticated, authorizeRoles("admin"), uploadCourse);
 courseRouter.post("/update-course:id", isAuthenticated, authorizeRoles("admin"), editCourse);
 courseRouter.post("/get-course:id",getSingleCourse);
+courseRouter.post("/get-courses",getAllCourses);
 
 export default courseRouter;
